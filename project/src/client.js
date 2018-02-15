25d7ccdb75713e8aac02b77cdfb1893817d9cc42
Kyra Drake <Kyra.Drake@bankerstoolbox.com>
2018-02-15 15:46:08 -0600

    client side - receives to do list from server

--- project/src/client.js
+++ project/src/client.js
@@ -1,11 +1,31 @@
-const net = require('net')
+// Establish client connection to server
+var io = require('socket.io-client')
+var socket = io('http://localhost:3000')
 
-// server host and port
-var host = 'localhost'
-var port = 3000
+var todos = {}
+
+socket.emit('data')
+socket.on('todos', (data) => {
+  todos = data
+  console.log('todos are ' + todos)
+})
 
 // connect client to server
-var client = new net.Socket()
-client.connect(port, host, () => console.log('Connected to server on port 3000!'))
+//var client = new net.Socket()
+//client.connect(port, host, () => console.log('Connected to server on port 3000!'))
 
-exports.client = client
+//client.write('data')
+
+//exports.client = client
+
+module.exports = {
+  getServerData: () => {
+    socket.emit('data')
+  },
+  addItem: (item) => {
+    // add item to todo data and send to server
+  },
+  removeItem: (item) => {
+    // remove item from todo data and send to server
+  },
+}
