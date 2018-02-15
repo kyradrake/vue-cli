71e69d9016f7125ce2aaff2371922a168d0ca5ae
Kyra Drake <Kyra.Drake@bankerstoolbox.com>
2018-02-15 15:45:42 -0600

    server side - pass to do list to client

--- projectServer/server.js
+++ projectServer/server.js
@@ -7,17 +7,28 @@ var app = express()
 var server = http.Server(app);
 var io = require('socket.io')(server)
 
+// Read data from JSON file
+var readData = fs.readFileSync('data.json')
+var serverData = JSON.parse(readData)
+var todos = serverData.todos
+
+// testing
+console.log('server data ' + serverData)
+console.log('nextToDoID is ' + serverData.nextToDoID)
+
 // Start listening for clients
 server.listen(3000)
 
 // Log to console when a client connects
-io.on('connection', function(socket) {
+io.on('connection', (socket) => {
   console.log('Client connected!')
-})
 
-// Read data from JSON file
-let readData = fs.readFileSync('data.json')
-let todos = JSON.parse(readData)
+  // Send JSON data to client when 'data' message is received
+  socket.on('data', () => {
+    console.log('Client requested data')
+    socket.emit('todos', JSON.stringify(todos))
+  })
+})
 
 // Write data to JSON file
 function updateData(data) {
